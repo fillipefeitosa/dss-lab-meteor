3760f473ab4fc3e3487fdaf1a379807614d768cf
Fillipe Feitosa <fillfeitosa@gmail.com>
2020-04-06 14:07:08 +0100

    Working layout structure

---
+++ client/lib/navbar.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {
+    "use strict";
+
+    var myNav = document.getElementById("nav-helper");
+
+    if (document.body.scrollTop >= 82 || document.documentElement.scrollTop >= 82) {
+
+        myNav.style.opacity = "1";
+        myNav.style.filter  = 'alpha(opacity=100)';
+
+    } else {
+
+        myNav.style.opacity = "0";
+        myNav.style.filter  = 'alpha(opacity=0)';
+
+    }
+};
+
+
