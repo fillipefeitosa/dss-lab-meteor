5a47773d69044bd96f053c27975606217c4a7538
Fillipe Feitosa <fillfeitosa@gmail.com>
2020-07-01 12:22:14 +0100

    Carto API: CRUD complete for tech

--- client/hooks/cartoDB.js
+++ client/hooks/cartoDB.js
@@ -3,7 +3,7 @@ import { TechnicalMap } from '/imports/api/technicalMap/technicalMap';
 
 
 // Autoform routines to call API after insert hook, Created for Tools and for TechMaps
-var insertDocHook = {
+var cartoDBhooks = {
   after: {
     insert: function(error, result){
       if(error){
@@ -25,28 +25,43 @@ var insertDocHook = {
       }
     }
   },
-  update: function(error, result){
-    if (error){
-      console.log(error);
-    } else {
-      let currentDoc = TechnicalMap.findOne({ _id: result });
-      Meteor.call('updateDocToCarto', {
-        doc: currentDoc,
-        docId: result,
-        typeOfCall: 'trabalhos_tecnicos'
-      }, function(error, success) { 
-        if (error) { 
-          console.log('error', error); 
-        } 
-        if (success) { 
-          console.log('Doc successfully updated on Carto DB');
-        } 
-      });
+}
+
+var cartoDBupdate = {
+  after: {
+    update: function(error, result){
+      if (error){
+        console.log(error);
+      } else {
+        let currentDoc = TechnicalMap.findOne({ _id: this.docId });
+        
+        Meteor.call('updateDocToCarto', {
+          doc: currentDoc,
+          docId: this.docId,
+          typeOfCall: 'trabalhos_tecnicos'
+        }, function(error, result) { 
+          if (error) { 
+            console.log('error', error); 
+          } 
+          if (result) {
+            // If result comes as answer from the server, is Because CARTO API found some kind of problem. 
+            console.log(result.error);
+          } else {
+            // Default Behavior
+            console.log('Doc successfully updated on Carto DB');
+          }
+        });
+      }
     }
   }
 }
 
-// A before Hook
+// Apply Hooks
 AutoForm.hooks({
-  insertTechinicalMapForm: insertDocHook,
+  insertTechinicalMapForm: cartoDBhooks,
+});
+
+AutoForm.hooks({
+  quickEditForm: cartoDBupdate,
 })
+// AutoForm.addHooks(['insertTechnicalMapForm'], cartoDBhooks);  
--- server/methods.js
+++ server/methods.js
@@ -38,7 +38,7 @@ Meteor.methods({
       {
         params:
         {
-          q: `DELETE FROM ${typeOfCall} WHERE mongo_idd='${docId}'`,
+          q: `DELETE FROM ${typeOfCall} WHERE mongo_id='${docId}'`,
           api_key: "3d5e5b8b0063217bfdfdacff263bd4edc24d871f"
         }
       }) 
@@ -47,7 +47,28 @@ Meteor.methods({
       return { error: error, errorMsg: error.response.content };
     }
   },
-  updateDocToCarto: function({docId, typeOfCall}) {
+  updateDocToCarto: function({doc, docId, typeOfCall}) {
     // Create
+    try {
+      let url = "http://fillipefeitosa.cartodb.com/api/v2/sql";
+      let docGeometry = `ST_SetSRID(ST_Point(${doc.geometry.coordinates[0]}, ${doc.geometry.coordinates[1]}),4326)`; 
+      const result = HTTP.call('POST', url, 
+      {
+        params: 
+        {
+          q: `UPDATE ${typeOfCall} SET
+          the_geom=${docGeometry}, _desc='${doc.properties.desc}',
+          name='${doc.properties.name}', _group='${doc.properties.group}',
+          link='${doc.properties.link}'
+          WHERE mongo_id='${docId}'
+          `,
+          api_key: "3d5e5b8b0063217bfdfdacff263bd4edc24d871f"
+        }
+      }
+      );
+    } catch (error) {
+      console.log(error);
+      return { error:error, errorMsg:error.response.content};
+    }
   } 
 });
